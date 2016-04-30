d8e33b37b874b0392c4f1a867d8a77143985834e
Benjamin Rosner <brosner@barnard.edu>
2016-04-30 16:00:34 -0400

    Initial Code Commit

---
+++ courseworks-nonrespondents.py
@@ -0,0 +1,90 @@
+import sys
+import time
+
+import requests
+import sendgrid
+
+login_uni = "XXXXX"
+password = "XXXXX"
+sendgrid_api_key = "XXXXX"
+evaluation_list = [14631289, 14468405, 14662773, 14648182, 14468178, 14468284, 14631268, 14631162, 14468601, 14631417,
+                   14631377]
+
+email_from = 'XXXXX'
+email_reply_to = 'XXXXX'
+email_recipients = ["XXXXX", "XXXXX", "XXXXX", "XXXXX"]
+
+######################################
+# Code - Do not edit below this line #
+######################################
+# This should end with the current evaluation ID
+cw_eval_uri = "https://courseworks.columbia.edu/direct/eval-reports/"
+
+# The parameter here is the evaluation ID
+cw_report_uri = "/report_view_report?mergeReports=true&evaluationId="
+cw_report_uri2 = "&showSettingsDetail=false&canSelectGroups=false&returnTo=0&external=false&settingsByEvaluation=true" \
+                 "&directView=false&viewmode=excluded&groupIds=" \
+                 "&command+link+parameters%26Submitting%2520control%3DviewReportTop=Submit"
+
+# No respondent URI, parameter here is evaluation ID.
+cw_non_respondent = '/csvNonTakersReport?templateId=12314653&evaluateeId=excluded&filename=non_takers.csv&evalId='
+
+web_worker = requests.session()
+datetime = time.strftime('%x %H:%M:%S')
+
+
+def login():
+    # Generic Login Procedure
+    payload = {
+        '_username': login_uni,
+        '_password': password
+    }
+    # Configure our session
+    # Login to CourseWorks.
+    web_worker.post('https://courseworks.columbia.edu/direct/session.json?', data=payload)
+    # Get our SESSION info.
+    response = web_worker.get('https://courseworks.columbia.edu/direct/session.json')
+    if response.status_code != 200:
+        print('Error: Received a non-200 response during login.')
+        sys.exit(1)
+
+    # Set reply to JSON and parse.
+    reply_json = response.json()
+    user_eid = reply_json['session_collection'][0]['userEid']
+    if user_eid != login_uni:
+        print('Error: Could not login as user ' + login_uni)
+        sys.exit(1)
+
+    print('Logged in as: ' + user_eid)
+
+
+def get_reports():
+    report = open('CourseWorks-NonTakersReport.csv', 'w')
+    report.write("Course ID, EMAIL, NAME\n")
+    for eval_id in evaluation_list:
+        eval_id = str(eval_id)
+        cw_preload_eval = ''.join([cw_eval_uri, eval_id, cw_report_uri, eval_id, cw_report_uri2])
+        web_worker.get(cw_preload_eval)
+        cw_fetch_csv = ''.join([cw_eval_uri, eval_id, cw_non_respondent, eval_id])
+        csv = web_worker.get(cw_fetch_csv)
+        report.write(csv.text)
+
+
+def send_email():
+    client = sendgrid.SendGridClient(sendgrid_api_key)
+    message = sendgrid.Mail()
+    for recipient in email_recipients:
+        message.add_to(recipient)
+    message.set_from(email_from)
+    message.set_replyto(email_reply_to)
+    message.set_subject("CourseWorks Evaluation Non-Takers Report as of " + datetime)
+    message.set_html("This is an automated message. Attached is the evaluation non-takers report.<br>"
+                     "Report was generated " + datetime + "<br>"
+                                                          "Contact Ben if you have questions.")
+    message.add_attachment('CourseWorks-NonTakersReport.csv', './CourseWorks-NonTakersReport.csv')
+    client.send(message)
+
+
+login()
+get_reports()
+send_email()
